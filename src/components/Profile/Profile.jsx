6456cd47d3c37e4bd66e38535ac051f71aa584ad
Evgeniy Boyko <jekaboyko0605@gmail.com>
2024-07-01 23:11:33 +0300

    add style to Profile component

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,25 +1,27 @@
+import s from './Profile.module.css';
+
 const Profile = ({ name, tag, location, image, stats: { followers, views, likes } }) => {
   return (
-    <div>
-      <div>
-        <img src={image} alt={name} />
-        <p>{name}</p>
-        <p>@{tag}</p>
-        <p>{location}</p>
+    <div className={s.profileCard}>
+      <div className={s.info}>
+        <img className={s.image} src={image} alt={name} />
+        <p className={s.username}>{name}</p>
+        <p className={s.tags}>@{tag}</p>
+        <p className={s.tags}>{location}</p>
       </div>
 
-      <ul>
-        <li>
-          <span>Followers</span>
-          <span>{followers}</span>
+      <ul className={s.stats}>
+        <li className={s.item}>
+          <span className={s.statsLabel}>Followers</span>
+          <span className={s.statsValue}>{followers}</span>
         </li>
-        <li>
-          <span>Views</span>
-          <span>{views}</span>
+        <li className={s.item}>
+          <span className={s.statsLabel}>Views</span>
+          <span className={s.statsValue}>{views}</span>
         </li>
-        <li>
-          <span>Likes</span>
-          <span>{likes}</span>
+        <li className={s.item}>
+          <span className={s.statsLabel}>Likes</span>
+          <span className={s.statsValue}>{likes}</span>
         </li>
       </ul>
     </div>
